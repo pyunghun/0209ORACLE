fb645ca5504780b617e96497242be55d937934b7
pyunghun cho <pyunghun120@gmail.com>
2023-02-10 18:23:02 +0900

    안녕하세요

---
+++ src/com/greedy/emp/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package com.greedy.emp;
+
+public class EmployeeDAO {
+
+	/* 구현 조건
+	 * 1. connection 생성은 jdbc 템플릿 사용
+	 * 2. query문은 xml 파일로 분리
+	 * 3. 쿼리문에 값을 전달해야 하는 경우는 preparedStatement 아니면 Statement 사용
+	 * 4. 한 행 정보는 dto에 담아 출력, 여러 행은 ArrayList */
+		/* 사번으로 직원 정보 조히 후 EmployeeDto에 담아 출력 */
+
+	public void findOneEmpByEmpId(String inputEmpId) {
+		
+		
+		
+	}
+}
